8dab92944b422790a32e5c4eeeb417e763270698
Ben Nolan <bnolan@gmail.com>
2026-06-01 11:21:48 +1200

    Update parcel.ts

--- src/parcel.ts
+++ src/parcel.ts
@@ -222,10 +222,10 @@ export default class Parcel extends TypedEventTarget<ParcelEventMap> {
     this.hardFeatureBounds = this.sandbox
       ? new BABYLON.BoundingBox(new BABYLON.Vector3(this.x1 - streetWidth, this.y1 - underHeight, this.z1 - streetWidth), new BABYLON.Vector3(this.x2 + streetWidth, this.y2 + overHeight, this.z2 + streetWidth), parent._worldMatrix)
       : new BABYLON.BoundingBox(
-          new BABYLON.Vector3(this.x1 - hardFeatureBound, this.y1 - hardFeatureBound, this.z1 - hardFeatureBound),
-          new BABYLON.Vector3(this.x2 + hardFeatureBound, this.y2 + hardFeatureBound, this.z2 + hardFeatureBound),
-          parent._worldMatrix,
-        )
+        new BABYLON.Vector3(this.x1 - hardFeatureBound, this.y1 - hardFeatureBound, this.z1 - hardFeatureBound),
+        new BABYLON.Vector3(this.x2 + hardFeatureBound, this.y2 + hardFeatureBound, this.z2 + hardFeatureBound),
+        parent._worldMatrix,
+      )
 
     // fix parcel offset, but leave enough for exterior signage
     const grace = 0.1
@@ -235,10 +235,10 @@ export default class Parcel extends TypedEventTarget<ParcelEventMap> {
     this.exteriorBounds = this.sandbox
       ? this.boundingBox
       : new BABYLON.BoundingBox(
-          new BABYLON.Vector3(this.x1 + offset - grace, this.y1 + offset - grace, this.z1 + offset - grace),
-          new BABYLON.Vector3(this.x2 + offset + grace, this.y2 + offset + grace, this.z2 + offset + grace),
-          parent._worldMatrix,
-        )
+        new BABYLON.Vector3(this.x1 + offset - grace, this.y1 + offset - grace, this.z1 + offset - grace),
+        new BABYLON.Vector3(this.x2 + offset + grace, this.y2 + offset + grace, this.z2 + offset + grace),
+        parent._worldMatrix,
+      )
 
     this._parcelBouncer = new ParcelBouncer(this)
     /**
@@ -1506,7 +1506,7 @@ export default class Parcel extends TypedEventTarget<ParcelEventMap> {
     if (window.graphic?.realisticLighting && this.field) {
       const lanterns = this.features.filter((f) => f.type === 'lantern') as LanternRecord[]
       // Y matches setVoxelMesh so voxel.ts pick/place math is correct
-      const off: [number, number, number] = [-this.width / 4, -(1 + this.ZFightingNudge), -this.depth / 4]
+      const off: [number, number, number] = [-this.width / 4 + 0.25, -0.25 + this.ZFightingNudge, -this.depth / 4 + 0.25]
       const { opaque, glass } = await buildCleanMesh(this.field, lanterns, this.scene, off, this.id)
       this.voxelMesh?.dispose()
       this.voxelMesh = opaque
